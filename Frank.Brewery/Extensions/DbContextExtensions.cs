d49da738ea6cde91ac11894e47d692d3b637b70a
Frank Haugen <frank.haugen@bilagos.no>
2020-03-01 20:57:07 +0100

    DbContext Extension

---
+++ Frank.Brewery/Extensions/DbContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Frank.Brewery.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static IEnumerable<T> AddAndSaveItems<T>(this DbContext context, IEnumerable<T> list)
+        {
+            var output = new List<T>();
+
+            foreach (var item in list)
+            {
+                var entry = context.Add(item);
+                output.Add((T)entry.Entity);
+            }
+
+            context.SaveChanges();
+
+            return output;
+        }
+    }
+}
